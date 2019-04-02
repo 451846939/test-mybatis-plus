e7f2ef8c73982cdf993247793885be7cfdf00d13
北洛 <beiluo@uoko.com>
2019-04-02 09:39:46 +0800

    修改注释

--- src/main/java/com/example/test/test/annotation/Copy.java
+++ src/main/java/com/example/test/test/annotation/Copy.java
@@ -3,15 +3,22 @@ package com.example.test.test.annotation;
 import java.lang.annotation.*;
 
 /**
- * 在需要复制的类上加上该注解可以使用 {@link CopyUtils}
+ * @description 在需要复制的类上加上该注解可以使用 {@link CopyUtils}
+ * @author beiluo
+ * @date 9:36 2019/4/2
+ * @version V1.0
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Copy {
     /**
-     * 是否复制null
-     * @return
+     * @description 是否复制null
+     * @author beiluo
+     * @date 9:36 2019/4/2
+     * @return boolean
+     * @param
+     * @version V1.0
      */
     boolean copyNull() default false;
 }
--- src/main/java/com/example/test/test/annotation/CopyFieldGroups.java
+++ src/main/java/com/example/test/test/annotation/CopyFieldGroups.java
@@ -3,7 +3,10 @@ package com.example.test.test.annotation;
 import java.lang.annotation.*;
 
 /**
- * {@link CopyField} 的组
+ * @description {@link CopyField} 的组
+ * @author beiluo
+ * @date 9:35 2019/4/2
+ * @version V1.0
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
