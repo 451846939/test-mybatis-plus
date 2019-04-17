2fe2839fc4a4f3f03a782d3e96dd1639240a2e36
北洛 <beiluo@uoko.com>
2019-04-17 18:32:50 +0800

    斐波那契数列

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -5,9 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,12 +16,14 @@ public class TestApplicationTests {
     }
 
     public static void main(String[] args) {
-        ArrayList<String> objects = new ArrayList<>();
-        objects.add("11");
-        objects.add("22");
-        List<Object> collect = objects.stream().map(object -> {
-            return null;
-        }).filter(o -> o!=null).collect(Collectors.toList());
-        System.out.println(collect);
+//        ArrayList<String> objects = new ArrayList<>();
+//        objects.add("11");
+//        objects.add("22");
+//        List<Object> collect = objects.stream().map(object -> {
+//            return null;
+//        }).filter(o -> o!=null).collect(Collectors.toList());
+//        System.out.println(collect);
+//        斐波那契数列
+        Stream.iterate(new int[] {0,1},t->new int[]{t[1],t[0]+t[1]}).limit(20).forEach(t-> System.out.println(t[0]+","+t[1]));
     }
 }
